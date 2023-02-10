2c6e2ad15d0ba864dfa83fa38519be08443a16f1
Gabriel Rocha <gabrielrochacontato1@gmail.com>
2023-02-10 10:12:24 -0300

    Atualização

--- cypress/e2e/frontTest.cy.js
+++ cypress/e2e/frontTest.cy.js
@@ -1,22 +1,19 @@
 /// <reference types="cypress" />
 
-describe('Front', () => {
+describe('realizar login de usuário', () => {
     it('deve realizar o login de um usuário', () => {
         cy.intercept('*/login')
-        cy.visit('https://front.serverest.dev/login')
-        cy.get('[data-testid="email"]').type('GabriellaChafir2023@qa.com.br')
-        cy.get('[data-testid="senha"]').type('teste12345')
-        cy.get('[data-testid="entrar"]').click()
-
-    });
-
-    it('deve realizar o login de um usuário', () => {
-        cy.intercept('*/login')
-        cy.visit('https://front.serverest.dev/login')
-        cy.get('[data-testid="email"]').type('GabriellaChafir2017@qa.com.br')
-        cy.get('[data-testid="senha"]').type('teste12345')
-        cy.get('[data-testid="entrar"]').click()
-
+        cy.visit('http://167.250.26.147:8080/sigAutomacao/app.html#/folha/pessoa')
+        cy.get('#usuario').type('automacao')
+        cy.get('#iPassword').type('1010')
+        cy.wait(5000)
+        cy.get('input.ng-scope').click()
+        cy.wait(5000)
+        cy.get('[nat="cadastroPessoaFolhaCodigoProximo"] > .fa').click()
+        cy.get(':nth-child(5) > pd-autocomplete.ng-pristine > .pd-autocomplete > .form-group > .input-group > .input-group-btn > .btn > .fa').click()
+        cy.get('#\31 675967806711-0-uiGrid-007H-cell > .ui-grid-cell-contents > .btn')
+        
+        
 
     });
 
