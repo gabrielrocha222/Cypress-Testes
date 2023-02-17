37fb1420c36d96d8269cfae9561f75517a1d6abe
Gabriel Rocha <gabrielrochacontato1@gmail.com>
2023-02-17 15:54:14 -0300

    Atualização

--- cypress/e2e/frontTest.cy.js
+++ cypress/e2e/frontTest.cy.js
@@ -9,10 +9,9 @@ describe('realizar login de usuário', () => {
         cy.wait(5000)
         cy.get('input.ng-scope').click()
         cy.wait(5000)
-        cy.get('[nat="cadastroPessoaFolhaCodigoProximo"] > .fa').click()
-        cy.get(':nth-child(5) > pd-autocomplete.ng-pristine > .pd-autocomplete > .form-group > .input-group > .input-group-btn > .btn > .fa').click()
-        cy.get('#\31 675967806711-0-uiGrid-007H-cell > .ui-grid-cell-contents > .btn')
-        
+        cy.get('#pdDivBody').scrollTo(0, 1000)
+        cy.get('[name="483inputAcDescricao"]').click().type('Gabriel')
+        cy.get('[class="btn btn-default form-control ui-select-toggle"]').clcik
         
 
     });
